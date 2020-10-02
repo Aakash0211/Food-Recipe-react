68dac4aeafb94b5b7ae3842c15f4f159075ac0b2
Aakash <akashkushi111@gmail.com>
2020-10-02 11:33:58 +0530

    Updates

---
+++ precache-manifest.e0d7b163f78eed9d6858b7288fd8bb06.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c7411d5af941f6ff44886b4da67221a",
+    "url": "/Food-Recipe-react/index.html"
+  },
+  {
+    "revision": "67409ae8077414cfbac2",
+    "url": "/Food-Recipe-react/static/css/main.ad06a21e.chunk.css"
+  },
+  {
+    "revision": "d56b1daee22fc6190aff",
+    "url": "/Food-Recipe-react/static/js/2.20cc933b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Food-Recipe-react/static/js/2.20cc933b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67409ae8077414cfbac2",
+    "url": "/Food-Recipe-react/static/js/main.fe0c87df.chunk.js"
+  },
+  {
+    "revision": "3f0dd82b0a7838f297a8",
+    "url": "/Food-Recipe-react/static/js/runtime-main.fc28f8cb.js"
+  }
+]);
